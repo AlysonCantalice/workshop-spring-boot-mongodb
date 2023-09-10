7946fac8c487b011f5ec11d2be099bdc3bbb42ad
Alyson Cantalice <alysoncantalice@gmail.com>
2023-09-10 00:29:47 -0300

    Create post comments

--- src/main/java/com/alysonca/workshopmongo/config/Instantiation.java
+++ src/main/java/com/alysonca/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.alysonca.workshopmongo.domain.Post;
 import com.alysonca.workshopmongo.domain.User;
 import com.alysonca.workshopmongo.dto.AuthorDTO;
+import com.alysonca.workshopmongo.dto.CommentDTO;
 import com.alysonca.workshopmongo.repository.PostRepository;
 import com.alysonca.workshopmongo.repository.UserRepository;
 
@@ -38,6 +39,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, Instant.parse("2018-03-23T10:21:05Z"), "Bom dia", "Acordei feliz hoje!",
 				new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", Instant.parse("2018-03-21T20:11:54Z"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", Instant.parse("2018-03-21T22:02:18Z"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", Instant.parse("2018-03-18T10:25:33Z"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
