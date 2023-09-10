52ea0873fa3cbbc082bd83f48229c15d66d63271
Alyson Cantalice <alysoncantalice@gmail.com>
2023-09-10 00:42:16 -0300

    Simple query consult

--- src/main/java/com/alysonca/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alysonca/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.alysonca.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.alysonca.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
